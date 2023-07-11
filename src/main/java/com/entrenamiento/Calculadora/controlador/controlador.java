eeb65e69fc55b66339440f77e2c6f1f7f03f4765
Esteban Pulgarin <esteban.pulgarin@quind.io>
2023-07-10 23:09:56 -0500

    version controller

--- src/main/java/com/entrenamiento/Calculadora/controlador/controlador.java
+++ src/main/java/com/entrenamiento/Calculadora/controlador/controlador.java
@@ -1,27 +1,12 @@
 package com.entrenamiento.Calculadora.controlador;
+
 import com.entrenamiento.Calculadora.modelo.resultado;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@RestController
-@RequestMapping("/version")
-public class VersionController {
-    @GetMapping(produces = "application/json; charset=UTF-8")
-    @ResponseBody
-    public ResponseEntity<String> version(){
-        return new ResponseEntity<>("app-name v#{Build.BuildNumber}#", HttpStatus.OK);
-    }
-}
+import org.springframework.http.MediaType;
 
 @CrossOrigin(origins = "*")
 @RestController
